636010a80913bb7a1d8659736fa5371c30bfa037
Shawn <wangcong26@hotmail.com>
2019-03-28 16:34:21 -0400

    Add files via upload
    Decorrelation using PCA

---
+++ Pearsonr/Decorrelate using PCA.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 16:33:22 2019
+
+@author: swang
+"""
+### Decompose correlation using PCA
+# Import PCA
+from sklearn.decomposition import PCA
+
+# Create PCA instance: model
+model = PCA()
+
+# Apply the fit_transform method of model to grains: pca_features
+pca_features = model.fit_transform(grains)
+
+# Assign 0th column of pca_features: xs
+xs = pca_features[:,0]
+
+# Assign 1st column of pca_features: ys
+ys = pca_features[:,1]
+
+# Scatter plot xs vs ys
+plt.scatter(xs, ys)
+plt.axis('equal')
+plt.show()
+
+# Calculate the Pearson correlation of xs and ys
+correlation, pvalue = pearsonr(xs, ys)
+
+# Display the correlation
+print(correlation)
